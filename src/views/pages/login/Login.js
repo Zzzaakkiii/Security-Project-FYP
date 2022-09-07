a47a3bb7d37e4023a16b128c00dbcac9a76105cc
Zaki Khalid <zaki.khalid@bykea.com>
2022-09-07 17:23:50 +0500

    env changes

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -63,13 +63,10 @@ const Login = () => {
                 <CCardBody className="text-center">
                   <div>
                     <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
+                    <p>Do not have an account? Register now!</p>
                     <Link to="/register">
                       <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Register Now!
+                        Register Now!!
                       </CButton>
                     </Link>
                   </div>
