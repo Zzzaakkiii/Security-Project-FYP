e0a68c3776db4e040fbe57395c6bc570196db855
Zaki Khalid <zaki.khalid@bykea.com>
2022-09-25 01:37:29 +0500

    added new lables

--- src/views/pages/Wallet/Wallet.js
+++ src/views/pages/Wallet/Wallet.js
@@ -10,6 +10,9 @@ import api from '../../../Services/DataControlService'
 
 const Wallet = () => {
     const [amount, setAmount] = useState(0)
+    const [approved, setApproved] = useState(0)
+    const [pending, setPending] = useState(0)
+    const [available, setAvailable] = useState(0)
 
     useEffect(() => {
         if (localStorage.getItem("role") === "user") {
@@ -20,22 +23,66 @@ const Wallet = () => {
                     },
                 });
 
-
                 setAmount(data.data.msg.wallet);
             };
 
+            const fetchAmountDetails = async () => {
+                const data = await api.get("v2/user/month/balance", {
+                    headers: {
+                        authorization: 'Bearer '.concat(localStorage.getItem("token")),
+                    },
+                });
+
+                setApproved(data.data.msg.approve_amount)
+                setAvailable(data.data.msg.remaining)
+                setPending(data.data.msg.pending_amount)
+            }
+
             fetchWallet()
+            fetchAmountDetails()
         }
     }, [amount])
 
     return (
-        <CRow className='d-flex flex-row align-items-center  min-vw-100'>
-            <CCol sm={6} lg={3}>
+        <CRow className='d-flex justify-content-center'>
+            <CCol sm={3} lg={4}>
                 <CWidgetStatsA
                     className="mb-4"
                     color="info"
                     value={`£ ${amount}`}
                     title="Wallet Amount"
+                />
+            </CCol>
+            <CCol sm={3} lg={4}>
+                <CWidgetStatsA
+                    className="mb-4"
+                    color="info"
+                    value={`£ 2000`}
+                    title="Monthly Limit"
+                />
+            </CCol>
+            <CCol sm={3} lg={4}>
+                <CWidgetStatsA
+                    className="mb-4"
+                    color="info"
+                    value={`£ ${available}`}
+                    title="Available Funds for request"
+                />
+            </CCol>
+            <CCol sm={3} lg={4}>
+                <CWidgetStatsA
+                    className="mb-4"
+                    color="info"
+                    value={`£ ${approved}`}
+                    title="Approved Funds"
+                />
+            </CCol>
+            <CCol sm={3} lg={4}>
+                <CWidgetStatsA
+                    className="mb-4"
+                    color="info"
+                    value={`£ ${pending}`}
+                    title="Pending Requested Funds"
                 />
             </CCol>
         </CRow>
